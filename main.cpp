ae94640148ff3e19b245eeb0243abd1129eea0bf
mohabstudent <99211618+mohabstudent@users.noreply.github.com>
2022-10-22 07:23:33 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,38 @@
+/*
+Name: codepro2
+PROB: barn1
+LANG: C++
+*/
+#include<bits/stdc++.h>
+#define en "\n"
+#define FF first
+#define SS second
+
+using namespace std;
+int M,S,C;
+vector<int>s;
+int main()
+{
+    ofstream fout ("barn1.out");
+    ifstream fin ("barn1.in");
+    // board ,total ,busy of total
+
+    fin>>M>>S>>C;
+    for(int i=0;i<C;i++)
+        {int t;fin>>t,s.push_back(t);}
+    sort(s.begin(),s.end());
+    int ans=S;
+    ans-=s[0]-1;
+    ans-=S-s.back();
+    for(int i=0;i<C-1;i++)
+    {
+        s[i]=s[i+1]-s[i]-1;
+    }
+    s.pop_back();
+    sort(s.rbegin(),s.rend());
+    for(int i=0;i<min(M-1,C-1);i++)
+    {
+        ans-=s[i];
+    }fout<<ans<<en;
+    return 0;
+}
